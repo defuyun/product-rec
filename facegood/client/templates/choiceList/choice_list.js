b6f7275d9e809bc6c5bc60f43441f910aa8d0b64
AlanYiNew <245655664@qq.com>
2016-04-29 22:10:30 -0700

    ui updating

---
+++ facegood/client/templates/choiceList/choice_list.js
@@ -0,0 +1,18 @@
+var choices =[
+	{
+		name:"Amercian apple",
+		description:"Some apples"
+	},
+	{
+		name:"Chinese banana",	
+		description:"Some bananas"
+	},
+	{
+		name:"Australian oranges",
+		description:"Some oranges"
+	}
+]
+
+Template.choiceList.helpers({
+	choices : choices
+});
