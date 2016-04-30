e982fd7d4c0ae97c43c4d8e6eb786e28f0a81c5a
AlanYiNew <245655664@qq.com>
2016-04-29 21:39:52 -0700

    temp ui

---
+++ facegood/client/templates/goodsList/goods_list.js
@@ -0,0 +1,18 @@
+var goods =[
+	{
+		name:"Amercian apple",
+		description:"Some apples"
+	},
+	{
+		name:"Chinese banana",	
+		description:"Some bananas"
+	},
+	{
+		name:"Australian oranges",
+		description:"Some oranges"
+	}
+]
+
+Template.goodsList.helpers({
+	goods : goods
+});
